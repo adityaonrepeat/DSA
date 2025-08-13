79a743161de1103ffa300e676354083dcaa758f0
Aditya Kumar Singh <adityasinghstuff@gmail.com>
2025-08-13 23:31:48 +0530

    Create Climbing_stairs.cpp

---
+++ Dynamic_Programming/Climbing_stairs.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/climbing-stairs/
+class Solution {
+public:
+    int dp[46];
+    int solve(int n){
+        
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n]!=-1) return dp[n];
+
+        return dp[n] = solve(n-1) + solve(n-2);
+    }
+    int climbStairs(int n) {
+        // vector<int> dp(44,-1);
+        memset(dp,-1,sizeof(dp));
+        return solve(n);
+    }
+};
